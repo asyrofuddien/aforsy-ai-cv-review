e51161c626523a2d2a2fc6bf94cc437a08397af6
Asyrofuddien <74283931+asyrofuddien@users.noreply.github.com>
2025-11-27 14:51:06 +0700

    Update model

--- src/models/atsPDF.model.ts
+++ src/models/atsPDF.model.ts
@@ -4,24 +4,29 @@ export interface ICvAts extends Document {
   code_id: mongoose.Types.ObjectId;
 }
 
-const CvAtsSchema = new Schema({
-  code_id: {
-    type: Schema.Types.ObjectId,
-    ref: 'Document',
-    required: true,
+const CvAtsSchema = new Schema(
+  {
+    code_id: {
+      type: Schema.Types.ObjectId,
+      ref: 'Document',
+      required: true,
+    },
+    html_file: {
+      type: String,
+    },
+    pdf_file: {
+      type: String,
+    },
+    html_text: {
+      type: String,
+    },
+    file_name: {
+      type: String,
+    },
   },
-  html_file: {
-    type: String,
-  },
-  pdf_file: {
-    type: String,
-  },
-  html_text: {
-    type: String,
-  },
-  file_name: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 export default mongoose.model<ICvAts>('ats_pdf', CvAtsSchema);
