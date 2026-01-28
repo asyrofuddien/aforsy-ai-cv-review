308b4dd5ace67b23363be1bbed2a444af72e871d
Asyrofuddien <asyrofuddien11@gmail.com>
2026-01-28 10:09:02 +0700

    Update code.model.ts

--- src/models/code.model.ts
+++ src/models/code.model.ts
@@ -6,6 +6,8 @@ export interface ICode extends Document {
 
 const CodeSchema = new Schema({
   code: { type: String, required: true },
+}, {
+  timestamps: true,
 });
 
 export default mongoose.model<ICode>('Code', CodeSchema);
